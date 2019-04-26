aaf8c86079c3b3ebd290b96d23eefbb8bc729865
vickybat01 <satyajit.ness@gmail.com>
2019-04-26 10:31:47 +0530

    Initial Commit Zuul Server

---
+++ ZuulService/src/main/java/com/att/microservices/GatewayServicePreFilter.java
@@ -0,0 +1,60 @@
+package com.att.microservices;
+
+import org.springframework.http.HttpStatus;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class GatewayServicePreFilter extends ZuulFilter {
+//We can do a ton of things here. Like creating a session and wrapping in a cookie, creating jwt and a lot of URL validations
+	@Override
+	public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        
+        String requestURI=ctx.getRequest().getRequestURI();
+        String remoteHost=ctx.getRequest().getRemoteAddr();
+        
+        if(	requestURI != null && requestURI.contains("/att/")) {
+        	ctx.addZuulRequestHeader("testHeader", "testHeader");
+        	return null;
+        }else{
+        	sendInvalidUrlResponse(ctx);
+        }
+        
+		return null;
+	}
+
+	
+	private void sendInvalidUrlResponse(RequestContext ctx)
+    {
+        ctx.setSendZuulResponse(false);
+        ctx.getResponse().setContentType("'application/json'");
+        ctx.setResponseStatusCode(HttpStatus.BAD_REQUEST.value());
+        String response = "{\"statusCode\": \"40001\", \"description\": \"Indicates that url is incorrect and missing att\"}";
+        ctx.setResponseBody(response);
+    }
+    
+	
+	
+	
+	
+	@Override
+	public boolean shouldFilter() {
+		
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		
+		return 0;
+	}
+
+	@Override
+	public String filterType() {
+		
+		return "pre";
+	}
+
+}
---
+++ ZuulService/src/main/java/com/att/microservices/ZuulServiceApplication.java
@@ -0,0 +1,57 @@
+package com.att.microservices;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+
+@SpringBootApplication
+@Controller
+@EnableZuulProxy
+@Configuration
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+	@Bean
+    public GatewayServicePreFilter gatewayServicePreFilter() {
+    return new GatewayServicePreFilter();
+    }
+	
+	
+	@Bean
+	public ZuulFilter zuulFilter(){
+		return new ZuulFilter() {
+			@Override
+			public String filterType() {
+				return "post";
+			}
+
+			@Override
+			public int filterOrder() {
+				return 999999;
+			}
+
+			@Override
+			public boolean shouldFilter() {
+				return true;
+			}
+
+			@Override
+			public Object run() {
+				final List<String> routingDebug = (List<String>) RequestContext.getCurrentContext().get("routingDebug");
+				routingDebug.forEach(System.out::println);
+				return null;
+			}
+		};
+	}
+}
